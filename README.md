1523e8216294b736ea7d945df233115948cd55b4
Eugene Trifonov <Eugene.Africansky@gmail.com>
2023-04-24 14:52:40 +0300

    read with limited buffer, write error in case of request issues

--- README.md
+++ README.md
@@ -1,38 +1,47 @@
 # go-tts
 
-Application that converts text-to-speech using google translate API, so it needs an internet connection to do so.
+Converts text-to-speech using google translate API, so it needs an internet connection to do so.
 
-Application can get text stream from stdin and create audio stream into stdout.
+The application can get text stream from stdin and create audio stream into stdout.
 
-<h1>Install</h1>
+Use `.` or `\n` (new line) to over a statement, including the last one.
+
+<h1>Installation</h1>
 
 ```
 go install github.com/go-tts/tts/cmd/tts@latest
 ```
-This command will build tts tool on your machine, so you could use it as `tts` further.
+This command will build `tts` tool on your machine, so you could use it as `tts` further.
 
-<h2>Use from terminal</h2>
+or run from source code:
 
-<h3>Play from terminal</h3>
+```
+git clone github.com/go-tts/tts
+cd tts
+go run cmd/tts/main.go "Hello world"
+```
+
+<h2>Play from terminal</h2>
 
 ```
 tts "Hello world"
 ```
+Such call suppose to pronounce a text passed to `tts`. If `-l` is not defined, `en-US` language will be used for pronunciation.
 
-<h3>Write to file</h3>
+<h2>Write to file</h2>
 
 ```
 tts -l=en -i=text_file.txt -o=audio_file.mp3
 tts -l en-US < text_file.txt > audio_file.mp3
 ```
 
-<h3>Use with pipe</h3>
+<h2>Use with pipe</h2>
 
 ```
 tts -l it "Muy bien. Chao." | ffmpeg -i - -filter:a "atempo=1.5" audio_file.mp3
 ```
 
-As an example, here `ffmpeg` increases the speech speed.
+As an example, here `ffmpeg` increases the speech speed by 1.5x.
 
 <h2>Use in your projects</h2>
 
@@ -50,12 +59,4 @@ audioIn := speech.FromTextStream(textIn, speech.LangUs)
 ```
 ```
 err := speech.WriteToAudioStream(textIn, audioOut, "it")
-```
-
-<h2>Run on your machine using golang</h2>
-
-```
-git clone github.com/go-tts/tts
-cd tts
-go run cmd/tts/main.go "Hello world"
 ```
--- pkg/speech/speech.go
+++ pkg/speech/speech.go
@@ -1,12 +1,30 @@
 package speech
 
 import (
-	"bufio"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
 	"net/url"
 )
+
+// queryTextLimit is the limit the query to google handles fine.
+// Usually response from google is Bad Request if the text size is above this limit.
+const queryTextLimit = 200
+
+func FromText(text, lang string) (io.ReadCloser, error) {
+	url := fmt.Sprintf("http://translate.google.com/translate_tts?ie=UTF-8&textlen=%d&client=tw-ob&q=%s&tl=%s", len(text), url.QueryEscape(text), lang)
+	response, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("failed to query google: %w", err)
+	}
+	if response.StatusCode != http.StatusOK {
+		defer response.Body.Close()
+		bodyText := readBody(response.Body)
+		return nil, fmt.Errorf("failed to query google: response status %d - %s: %s", response.StatusCode, http.StatusText(response.StatusCode), bodyText)
+	}
+	return response.Body, nil
+}
 
 func FromTextStream(textIn io.Reader, lang string) io.ReadCloser {
 	r, w := io.Pipe()
@@ -21,36 +39,132 @@ func FromTextStream(textIn io.Reader, lang string) io.ReadCloser {
 }
 
 func WriteToAudioStream(textIn io.Reader, audioOut io.Writer, lang string) error {
-	input := bufio.NewScanner(textIn)
-	for input.Scan() {
-		text := input.Text()
-		if text == "" {
-			continue
+	readBuf := make([]byte, queryTextLimit)
+	var textBuf []byte
+	for {
+		n, err := textIn.Read(readBuf)
+		if errors.Is(err, io.EOF) {
+			for len(textBuf) > 0 {
+				textBuf = trimSpaces(textBuf)
+				textBuf, err = writeToAudioStream(textBuf, audioOut, lang)
+				if err != nil {
+					return fmt.Errorf("failed to write audio: %w", err)
+				}
+			}
+			return nil
 		}
-
-		audioIn, err := FromText(input.Text(), lang)
 		if err != nil {
-			return err
+			return fmt.Errorf("failed to read input text: %w", err)
 		}
 
-		if _, err := io.Copy(audioOut, audioIn); err != nil {
-			audioIn.Close()
-			return err
-		}
-
-		if err := audioIn.Close(); err != nil {
-			return err
+		textBuf = append(textBuf, readBuf[:n]...)
+		textBuf = trimSpaces(textBuf)
+		for len(textBuf) >= queryTextLimit {
+			textBuf, err = writeToAudioStream(textBuf, audioOut, lang)
+			if err != nil {
+				return fmt.Errorf("failed to write audio: %w", err)
+			}
+			textBuf = trimSpaces(textBuf)
 		}
 	}
-
-	return nil
 }
 
-func FromText(text, lang string) (io.ReadCloser, error) {
-	url := fmt.Sprintf("http://translate.google.com/translate_tts?ie=UTF-8&total=1&idx=0&textlen=%d&client=tw-ob&q=%s&tl=%s", len(text), url.QueryEscape(text), lang)
-	response, err := http.Get(url)
+func writeToAudioStream(textBuf []byte, audioOut io.Writer, lang string) ([]byte, error) {
+	if len(textBuf) == 0 {
+		return textBuf, nil
+	}
+
+	var text string
+	textBuf, text = scanText(textBuf)
+
+	audioIn, err := FromText(text, lang)
 	if err != nil {
 		return nil, err
 	}
-	return response.Body, err
+
+	if _, err := io.Copy(audioOut, audioIn); err != nil {
+		audioIn.Close()
+		return nil, err
+	}
+
+	if err := audioIn.Close(); err != nil {
+		return nil, err
+	}
+	return textBuf, nil
+}
+
+// readBody returns body from response if it exists,
+// or error if there is an error.
+func readBody(responseBody io.Reader) string {
+	body, err := io.ReadAll(responseBody)
+	if errors.Is(err, io.EOF) {
+		return "<empty response>"
+	}
+	if err != nil {
+		return err.Error()
+	}
+	if len(body) == 0 {
+		return "<empty response>"
+	}
+	return string(body)
+}
+
+// scanText scans what part of buffer can be sent to request,
+// and which part needs to be added before sending.
+func scanText(buf []byte) ([]byte, string) {
+	if len(buf) < queryTextLimit {
+		text := string(buf)
+		return buf[:0], text
+	}
+
+	separatorPlace := -1
+	for i := queryTextLimit - 1; i >= 0; i-- {
+		switch buf[i] {
+		case '.', ',', '!', '?', ';', ':':
+			separatorPlace = i
+			goto found
+		case '\n':
+			// exclude new line byte
+			separatorPlace = i - 1
+			if i > 1 && buf[i-2] == '\r' {
+				separatorPlace = i - 2
+			}
+			goto found
+		case ' ':
+			if separatorPlace < 0 {
+				// case, when there is no sign of statement over
+				separatorPlace = i
+			}
+		}
+	}
+found:
+	if separatorPlace < 0 {
+		// case when buffer contains one huge word
+		separatorPlace = queryTextLimit
+	}
+	text := string(buf[:separatorPlace+1])
+	copy(buf, buf[separatorPlace+1:])
+	return buf[:len(buf)-separatorPlace-1], text
+}
+
+// trimSpaces removes spaces in the beginning of the buffer
+// to avoid sending spaces via the internet.
+func trimSpaces(buf []byte) []byte {
+	cutFrom := -1
+	for i := 0; i < len(buf); i++ {
+		switch buf[i] {
+		case ' ', '\r', '\n':
+			cutFrom = i + 1
+		default:
+			goto out
+		}
+	}
+out:
+	if cutFrom < 0 {
+		return buf
+	}
+	if cutFrom == len(buf) {
+		return nil
+	}
+	return buf[cutFrom:]
 }
